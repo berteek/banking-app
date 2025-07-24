7d167b57d7305eaf81ff409408962fe154992d58
berteek <merzinalbert@gmail.com>
2025-07-24 23:06:38 +0400

    add logging

--- src/main/java/com/berteek/bankingapp/api/advice/GlobalExceptionHandler.java
+++ src/main/java/com/berteek/bankingapp/api/advice/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.berteek.bankingapp.api.advice;
 
+import com.berteek.bankingapp.api.controller.WalletController;
 import com.berteek.bankingapp.service.model.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -13,8 +16,11 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Result> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        logger.error(e.getMessage());
         BindingResult bindingResult = e.getBindingResult();
         StringBuilder errors = new StringBuilder();
         for (FieldError fieldError : bindingResult.getFieldErrors()) {
@@ -30,6 +36,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Result> exception(Exception e) {
+        logger.error(e.getMessage());
         return ResponseEntity
                 .internalServerError()
                 .body(new Result(Result.Status.FAILURE, "Unexpected behavior", null));
--- src/main/java/com/berteek/bankingapp/api/controller/WalletController.java
+++ src/main/java/com/berteek/bankingapp/api/controller/WalletController.java
@@ -6,6 +6,8 @@ import com.berteek.bankingapp.service.model.WalletTransaction;
 import java.util.UUID;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class WalletController {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(WalletController.class);
     private final WalletService walletService;
 
     @Autowired
     public WalletController(WalletService walletService) {
         this.walletService = walletService;
+        logger.info("WalletController created");
     }
 
     @PostMapping("/wallet")
     public ResponseEntity<Result> makeWalletTransaction(@Valid @RequestBody WalletTransaction walletTransaction) {
+        logger.info("Wallet transaction: {}", walletTransaction);
+
         if (!walletTransaction.isValid()) {
+            logger.info("Wallet transaction is invalid");
             return ResponseEntity
                     .badRequest()
                     .body(new Result(Result.Status.FAILURE, "Invalid request body", null));
@@ -38,20 +44,26 @@ public class WalletController {
 
         Result result;
         if (walletTransaction.getOperationType().equals(WalletTransaction.OPERATION_DEPOSIT)) {
+            logger.info("Wallet transaction deposit");
             result = walletService.deposit(walletTransaction.getWalletId(), walletTransaction.getAmount());
         } else if (walletTransaction.getOperationType().equals(WalletTransaction.OPERATION_WITHDRAW)) {
+            logger.info("Wallet transaction withdraw");
             result = walletService.withdraw(walletTransaction.getWalletId(), walletTransaction.getAmount());
         } else {
+            logger.info("Wallet transaction not supported");
             return ResponseEntity
                     .internalServerError()
                     .body(new Result(Result.Status.FAILURE, "Internal server error", null));
         }
 
         if (result.getStatus().equals(Result.Status.FAILURE)) {
+            logger.info("Wallet transaction failed");
             return ResponseEntity
                     .badRequest()
                     .body(result);
         }
+
+        logger.info("Wallet transaction succeeded");
         return ResponseEntity
                 .ok()
                 .body(result);
@@ -59,10 +71,13 @@ public class WalletController {
 
     @GetMapping("/wallets/{id}")
     public ResponseEntity<Result> getWalletBalance(@PathVariable UUID id) {
+        logger.info("Wallet transaction id: {}", id);
         Result result = walletService.findById(id);
         if (result.getStatus().equals(Result.Status.SUCCESS)) {
+            logger.info("Wallet transaction successful");
             return ResponseEntity.ok().body(result);
         }
+        logger.info("Wallet transaction failed");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
     }
 }
